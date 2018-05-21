cff41f03419c55606421590a921a40d6a354bb71
Tonya Martin <tonya.martin@pnnl.gov>
2018-05-21 13:49:58 -0700

    testing bash brackets

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,4 +5,14 @@ echo "in build script"
 echo "MYVAR $MYVAR"
 echo "MYSECRETVAR $MYSECRETVAR"
 echo "DOCKER_PROJECT $DOCKER_PROJECT"
+
+
+if [[ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]]; then
+  echo "here i am in double brackets"
+fi
+
+if [ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]; then
+  echo "here i am in single brackets"
+fi
+
 ./this.py
