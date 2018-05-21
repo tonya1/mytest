023d6e1a0f509e1381682e3fa3c6639f3e26d2d2
Tonya Martin <tonya.martin@pnnl.gov>
2018-05-21 13:54:02 -0700

    removed double brackets

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,13 +6,13 @@ echo "MYVAR $MYVAR"
 echo "MYSECRETVAR $MYSECRETVAR"
 echo "DOCKER_PROJECT $DOCKER_PROJECT"
 
-
-if [[ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]]; then
-  echo "here i am in double brackets"
-fi
-
 if [ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]; then
   echo "here i am in single brackets"
 fi
+
+#if [[ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]]; then
+#  echo "here i am in double brackets"
+#fi
+
 
 ./this.py
