a0bff3a82b1b50fdbff12cd9286423643479c4ee
Tonya Martin <tonya.martin@pnnl.gov>
2018-09-04 17:00:06 -0700

    Modified travis for failure testing

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-echo "---------------"
-echo "in build script"
-echo "MYVAR $MYVAR"
-echo "MYSECRETVAR $MYSECRETVAR"
-echo "DOCKER_PROJECT $DOCKER_PROJECT"
+TAG="$TRAVIS_BRANCH"
 
-if [ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]; then
-  echo "here i am in single brackets"
-fi
-
-#if [[ -n "$DOCKER_PROJECT" -a "$MYSECRETVAR" ]]; then
-#  echo "here i am in double brackets"
-#fi
+ORG=`echo $DOCKER_PROJECT | tr '[:upper:]' '[:lower:]'`
+ORG="${ORG:-tonyamartin}"
+ORG="${ORG:+${ORG}/}"
+IMAGE="${ORG}mytest"
+TIMESTAMP=`date +'%y%m%d%H'`
+GITHASH=`git log -1 --pretty=format:"%h"`
 
 # parse options
 while getopts bp option ; do
   case $option in
     b) # Pass gridappsd tag to docker-compose
       # Docker file on travis relative from root.
-      docker build -t test .
+      docker build -t ${IMAGE}:${TIMESTAMP}_${GITHASH} .
+      status=$?
+      echo "build status $status"
+
       ;;
     p) # Pass gridappsd tag to docker-compose
-      #if [ -n "$TAG" -a -n "$ORG" ]; then
-      #  echo "docker push ${IMAGE}:${TIMESTAMP}_${GITHASH}"
-      #  echo "docker push ${IMAGE}:$TAG"
-      #fi
-      ./this.py
+      docker images
+      if [ -n "$TAG" -a -n "$ORG" ]; then
+        # Get the built container name, for builds that cross the hour boundary
+        CONTAINER=`docker images --format "{{.Repository}}:{{.Tag}}" ${IMAGE}`
+        echo "$CONTAINER"
+
+        echo "docker push ${CONTAINER}"
+        docker push ${CONTAINER}
+        status=$?
+        echo "push status $status"
+
+        echo "docker tag ${CONTAINER} ${IMAGE}:$TAG"
+        docker tag ${CONTAINER} ${IMAGE}:$TAG
+        status=$?
+        echo "tag status $status"
+
+        echo "docker push ${IMAGE}:$TAG"
+        docker push ${IMAGE}:$TAG
+        status=$?
+        echo "push status $status"
+      fi
       ;;
     *) # Print Usage
       usage
