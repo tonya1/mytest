4a9632aaf10f7a64dd4fa6acb1dc24647804b99f
tonya1 <tonya.martin@pnnl.gov>
2019-05-22 13:06:26 -0400

    Updated travis git settings

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,7 +9,7 @@ IMAGE="${ORG}mytest"
 TIMESTAMP=`date +'%y%m%d%H'`
 GITHASH=`git log -1 --pretty=format:"%h"`
 
-git checkout -b $TRAVIS_BRANCH
+#git checkout -b $TRAVIS_BRANCH
 
 git status
 
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 #comment
 FROM busybox
 
+RUN git rev-parse --abbrev-ref HEAD
+
 ADD this.py /
